c9c7476470f41261571b00fc321dc10815b2a548
Henrique <henrique.c.ladeira@gmail.com>
2022-01-23 21:34:16 -0300

    chore: config iOS project

--- ios/scratch/AppDelegate.swift
+++ ios/scratch/AppDelegate.swift
@@ -6,31 +6,44 @@
 //
 
 import UIKit
+import React
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+    var bridge: RCTBridge?
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        let viewBridge = createBridgeIfNeeded()
+        let rootView: RCTRootView = RCTRootView(
+            bridge: viewBridge,
+            moduleName: "scratch",
+            initialProperties: nil)
+
+        self.window = UIWindow.init()
+        let rootViewController = UIViewController.init()
+        rootViewController.view = rootView
+        self.window!.rootViewController = rootViewController
+        
         return true
     }
 
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    func createBridgeIfNeeded() -> RCTBridge {
+        if bridge == nil {
+            bridge = RCTBridge.init(delegate: self, launchOptions: nil)
+        }
+        return bridge!
     }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
 
 }
 
+extension AppDelegate: RCTBridgeDelegate {
+    func sourceURL(for bridge: RCTBridge!) -> URL! {
+        #if DEBUG
+            return URL(string: "http://localhost:8081/index.bundle?platform=ios")
+        #else
+            return Bundle.main.url(forResource: "main", withExtension: "jsbundle")
+        #endif
+    }
+}
